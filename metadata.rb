f89ef328bd042468acb0c8c23c6151b95fce8b6d
Alex Knoll <alex@newmediadenver.com>
2014-05-06 16:18:09 -0600

    re-order metadata depends

--- metadata.rb
+++ metadata.rb
@@ -15,5 +15,5 @@ provides 'nmddatabase::useraccess'
 recipe 'nmddatabase::useraccess', 'Gives specific users access to a database.'
 
 depends 'chef-client', '= 3.2.2'
-depends 'database', '= 2.1.8'
 depends 'mysql', '= 5.2.4'
+depends 'database', '= 2.1.8'
